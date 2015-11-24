d3847d8fd360fa47b9f65a95049dc7c68c7db7d4
Alyssa Diaz <alycit@alyssas-mbp.attlocal.net>
2015-11-23 20:49:31 -0600

    completed through lecture 32

--- todo/src/app.jsx
+++ todo/src/app.jsx
@@ -2,23 +2,38 @@ var React = require('react');
 var ReactFire = require('reactfire');
 var Firebase = require('firebase');
 var Header = require('./header');
+var List = require('./list');
 var rootUrl = 'https://intense-torch-6825.firebaseio.com/'
 
 var App = React.createClass({
   mixins: [ ReactFire ],
   componentWillMount: function(){
-    this.bindAsObject(new Firebase(rootUrl + 'items'), 'items');
+    fb = new Firebase(rootUrl + 'items');
+    this.bindAsObject(fb, 'items');
+    fb.on('value', this.handleDataLoaded);
+  },
+  getInitialState: function() {
+    return {
+      loaded: false,
+      items: {}
+    }
   },
   render: function() {
-    console.log(this.state);
     return <div className="row panel panel-default">
       <div className="col-md-8 col-md-offset-2">
         <h2 className="text-center">
           To-Do List
         </h2>
-        <Header />
+        <Header itemsStore={this.firebaseRefs.items} />
+        <hr />
+        <div className={"content " + (this.state.loaded ? 'loaded' : '')}>
+          <List items={this.state.items} />
+        </div>
       </div>
     </div>
+  },
+  handleDataLoaded: function() {
+    this.setState({loaded: true});
   }
 });
 
