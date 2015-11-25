984bdea6a83727227472a03e4b73504fed0db2d7
Alyssa Diaz <alycit@alyssas-mbp.attlocal.net>
2015-11-24 19:22:51 -0600

    completed lecture 46

--- imgur-client/src/utils/api.jsx
+++ imgur-client/src/utils/api.jsx
@@ -2,12 +2,14 @@ var Fetch = require('whatwg-fetch');
 var rootUrl = 'https://api.imgur.com/3/';
 var apiKey = 'fa23abf1b4d03c5';
 
-module.exports = {
+module.exports = window.api = {
   get: function(url) {
     return fetch(rootUrl + url, {
       headers: {
-        'Authorization': 'Client-ID ' + apiKey;
+        'Authorization': 'Client-ID ' + apiKey
       }
+    }).then(function(response){
+      return response.json();
     });
   }
 }
