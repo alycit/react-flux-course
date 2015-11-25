517091e02748a4bef6b23a562094cd65efe768c2
Alyssa Diaz <alycit@alyssas-mbp.attlocal.net>
2015-11-24 19:29:07 -0600

    completed lecture 47

--- imgur-client/src/components/main.jsx
+++ imgur-client/src/components/main.jsx
@@ -1,9 +1,17 @@
 var React = require('react');
 var Header = require("./header");
+var TopicList = require("./topic-list");
 
 module.exports = React.createClass({
   render: function() {
     return <Header />
-    {this.props.children}
+    {this.content()}
+  },
+  content: function() {
+    if(this.props.children) {
+      return this.props.children;
+    } else {
+      return <TopicList />
+    }
   }
 });
